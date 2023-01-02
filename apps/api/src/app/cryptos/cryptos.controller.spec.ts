48c600b7acae5de63be6e1d1c043d69810739ce3
Mohamad Alabudi <mohamadalabudi42@gmail.com>
2023-01-02 17:15:02 -0500

    added unit test for cryptos api

--- apps/api/src/app/cryptos/cryptos.controller.spec.ts
+++ apps/api/src/app/cryptos/cryptos.controller.spec.ts
@@ -1,22 +1,36 @@
+import { HttpModule, HttpService } from '@nestjs/axios';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CryptosController } from './cryptos.controller';
 import { CryptosService } from './cryptos.service';
 
-describe('AppController', () => {
+describe('CryptosController', () => {
   let cryptosController: CryptosController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
       controllers: [CryptosController],
-      providers: [CryptosService],
+      providers: [
+        CryptosService,
+        {
+          provide: HttpService,
+          useValue: {
+            get: jest.fn(),
+          },
+        }
+      ],
     }).compile();
 
     cryptosController = app.get<CryptosController>(CryptosController);
+    
+    jest.resetAllMocks();
   });
 
-  /*describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(cryptosController.getHello()).toBe('Hello World!');
+  describe('Get', () => {
+    it('should call CoinMarketCap API', () => {
+      jest.spyOn(cryptosController, 'getAllCryptos');
+
+      expect(cryptosController.getAllCryptos()).toBeCalled();
     });
-  });*/
+  });
 });
--- apps/api/src/app/cryptos/cryptos.service.ts
+++ apps/api/src/app/cryptos/cryptos.service.ts
@@ -8,14 +8,11 @@ export class CryptosService {
   private readonly url =
     'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=200&convert=USD';
 
-  /*private readonly headersRequest: {
-    'X-CMC_PRO_API_KEY': 'f7298201-21c0-489e-a014-33e8d5061c0b';
-  };*/
-
   private readonly logger = new Logger(CryptosService.name);
 
   constructor(private readonly httpService: HttpService) {}
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   getCryptos(): Observable<AxiosResponse<any>> {
     return this.httpService
       .get(this.url, {
