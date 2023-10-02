62a90269ed1bf1785964d90b33ecdf977acb448a
Mohamad Alabudi <mohamadalabudi42@gmail.com>
2023-10-01 23:45:38 -0400

    Fixed CORS

--- apps/asset-prices/src/app/crypto.service.ts
+++ apps/asset-prices/src/app/crypto.service.ts
@@ -9,7 +9,8 @@ export class CryptoService {
   constructor(private http: HttpClient) { }
 
   getCryptos() {
-    const prodURL = 'https://asset-prices-api-production.up.railway.app/';
+    //const prodURL = 'https://asset-prices-api-production.up.railway.app/';
+    const prodURL = 'http://localhost:3333/';
     return this.http.get(prodURL + 'api/cryptos');
   }
 }
